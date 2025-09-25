6ef9a4c889dd163e389bd91d2cee682858314945
raulnu <raul.navarro.uribe@gmail.com>
2025-09-25 10:13:50 +0200

    fixed layout

--- src/shared/components/ui/organisms/Header/header.d.ts
+++ src/shared/components/ui/organisms/Header/header.d.ts
@@ -1,4 +1,4 @@
-export interface INavigationProps {
+export interface IHeaderProps {
   isHamburgerActive: boolean;
   toggleHamburger: () => void;
 }
--- src/shared/components/ui/template/Layout/Layout.tsx
+++ src/shared/components/ui/template/Layout/Layout.tsx
@@ -1,17 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Outlet } from "react-router";
 import Header from "../../organisms/Header/Header";
 import Footer from "../../organisms/Footer/Footer";
 import "./layout.module.scss";
+import Navigation from "../../molecules/Navigation/Navigation";
 
 interface LayoutProps {
   children?: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [isHamburgerActive, setIsHamburgerActive] = useState(false);
+  const toggleHamburger = () => {
+    setIsHamburgerActive(!isHamburgerActive);
+  };
   return (
     <div className="layout">
-      <Header />
+      <Header
+        isHamburgerActive={isHamburgerActive}
+        toggleHamburger={toggleHamburger}
+      />
+      <Navigation
+        isHamburgerActive={isHamburgerActive}
+        toggleHamburger={toggleHamburger}
+      />
       <main className="layout__main">{children || <Outlet />}</main>
       <Footer />
     </div>
